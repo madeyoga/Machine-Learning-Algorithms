ecdb942bd0ef280cb0cbd8839abb92ff77937cfe
MadeYoga <vngla21@gmail.com>
2019-01-04 10:31:49 +0630

    add example distances

---
+++ examples/example_distances.py
@@ -0,0 +1,14 @@
+from mla.base.distances import *
+
+A = [176, 72]
+B = [154, 85]
+C = [182, 75]
+D = [167, 98]
+E = [158, 65]
+Q = [160, 78]
+
+print(euclidean_distance(A, Q))
+print(euclidean_distance(B, Q))
+print(euclidean_distance(C, Q))
+print(euclidean_distance(D, Q))
+print(euclidean_distance(E, Q))
