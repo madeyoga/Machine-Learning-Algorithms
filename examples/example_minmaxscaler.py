070f254e4f7d8ddcfb5299efba21277be4347712
MadeYoga <vngla21@gmail.com>
2019-01-16 21:44:29 +0630

    update import

--- examples/example_minmaxscaler.py
+++ examples/example_minmaxscaler.py
@@ -1,4 +1,4 @@
-from mla.algos.Preprocessing.Scaler import *
+from mla.algos.Preprocessing.Scaler import MinMaxScaler
 
 scaler = MinMaxScaler()
 print(scaler.transform([3, 1, 5, 2, 1, 6, 7, 2, 1, 4]))
