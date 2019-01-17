57115e07fe249645dba1eaaba7fac09452eacd11
MadeYoga <vngla21@gmail.com>
2019-01-17 11:00:30 +0630

    remove examples from repo

--- examples/example_labelencoder.py
+++
@@ -1,5 +0,0 @@
-from mla.algos.Preprocessing.LabelEncoder import LabelEncoder
-
-le = LabelEncoder()
-le.fit(['x', 'x', 'x', 'y', 'z'])
-print(le.transform(['x', 'x', 'x', 'y', 'z']))
--- examples/example_minmaxscaler.py
+++
@@ -1,4 +0,0 @@
-from mla.algos.Preprocessing.Scaler import MinMaxScaler
-
-scaler = MinMaxScaler()
-print(scaler.transform([3, 1, 5, 2, 1, 6, 7, 2, 1, 4]))
