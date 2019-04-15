1195ce289e22e4044123751cf0bab11ab744ef19
MadeYoga <vngla21@gmail.com>
2019-04-16 00:41:11 +0700

    Add Z-score & Decimal Scaler class.

--- pymla/preprocessing/scaler.py
+++ pymla/preprocessing/scaler.py
@@ -2,7 +2,12 @@ from pymla.preprocessing.base import BasePreprocessing
 from pymla.preprocessing.base import np
 
 class MinMaxScaler(BasePreprocessing):
+    """
+    MinMax-Normalization.
+    v' = (v - min) * (new_max - new_min) / (max - min) + new_min.
     
+    currently can only scale value, 0 to 1.
+    """
     def transform(self, array : np.array):
         max_val = np.max(array)
         min_val = np.min(array)
@@ -11,3 +16,20 @@ class MinMaxScaler(BasePreprocessing):
         scaled_values = top / bott
         return scaled_values
     
+class ZScoreScaler(BasePreprocessing):
+    """
+    Z-Score Normalization.
+    v' = (v - mean) / std_dev.
+    """
+    
+    def transform(self, array : np.array):
+        return
+
+class DecimalScaler(BasePreprocessing):
+    """
+    Decimal Scaler.
+    v' = v / 10^j
+    """
+
+    def transform(self, array : np.array):
+        return
